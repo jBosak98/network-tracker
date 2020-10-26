bc71bd8fd5c4449bece196469aee33578beb5c34
jBosak <jbosak98@gmail.com>
2020-10-26 13:05:17 +0100

    refactored load device

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 extern crate pcap;
 extern crate argparse;
 
-use pcap::{Capture, Device, Active, Savefile};
+use pcap::{Capture, Device, Active, Savefile, Error};
 
-fn load_device<'a>(target:&str, requested_device:&'a mut Device, vector_devices: &'a Vec<Device>){
-    for device in vector_devices {
-        match device {
-            _ => {
-                println!("network: {}, {:?}", device.name, device.desc);
-                if &*device.name == target {
-                    requested_device.name = device.name.clone();
-                    requested_device.desc = device.desc.clone();
+fn load_device<'a>(target:&str, requested_device:&'a mut Device, devices: &'a Result<Vec<Device>,Error>){
+    match devices {
+        Ok(vector_devices) => {
+            for device in vector_devices {
+                match device {
+                    _ => {
+                        println!("network: {}, {:?}", device.name, device.desc);
+                        if &*device.name == target {
+                            requested_device.name = device.name.clone();
+                            requested_device.desc = device.desc.clone();
+                        }
+                    }
                 }
             }
+        } Err(_) => {
+            println!("Device not found");
+            std::process::exit(1)
         }
     }
+    println!("\ndetected target device: {}", requested_device.name);
 }
 
 fn get_file_to_save(filename:&str, capture:&Capture<Active>) -> Savefile {
@@ -31,15 +39,7 @@ fn main() {
     let target = "wlp3s0";
     let devices = Device::list();
     let mut requested_device : Device = Device::lookup().unwrap();
-    match devices {
-        Ok(vector_devices) => {
-            load_device(&target, &mut requested_device, &vector_devices);
-        } Err(_) => {
-            println!("Device not found");
-            std::process::exit(1)
-        }
-    }
-    println!("\ndetected target device: {}", requested_device.name);
+    load_device(&target, &mut requested_device, &devices);
 
     let mut capture = Capture::from_device(requested_device)
         .unwrap()
